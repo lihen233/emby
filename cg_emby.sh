fec2c9e61164cbbf45b6bf419da8877ba4448659
lihen233 <68227496+lihen233@users.noreply.github.com>
2021-07-05 16:17:21 +0200

    Update cg_emby.sh

--- cg_emby.sh
+++ cg_emby.sh
@@ -68,34 +68,34 @@ check_emby() {
 
 ################## 备份emby ##################
 bak_emby() {
-  check_emby
   remote_choose
-  systemctl stop emby-server #结束 emby 进程
-  rm -rf /var/lib/emby/cache/* #清空cache
-  cd /var/lib && tar -cvf emby_bak_"$(date "+%Y-%m-%d")".tar emby #打包/var/lib/emby
-  rclone move emby_bak_"$(date "+%Y-%m-%d")".tar "$my_remote": -vP #上传gd
-  systemctl start emby-server
+  systemctl stop jellyfin-service #结束 jellyfin 进程
+  #rm -rf /var/lib/jellyfin/.cache/* #清空cache
+  cd /var/lib && tar -cvf jellyfin_bak_"$(date "+%Y-%m-%d")".tar jellyfin #打包/var/lib/jellyfin
+  rclone move jellyfin_bak_"$(date "+%Y-%m-%d")".tar "$my_remote":  -vP #上传文件
+  systemctl start jellyfin-service
+  echo -e "${curr_date} [INFO] jellyfin备份完毕."
 }
 
 ################## 还原emby ##################
 revert_emby() {
-    check_emby
     remote_choose
-    rclone lsf "$my_remote":/ --include 'emby_bak*' --files-only -F "pt" | sed 's/ /_/g;s/\;/    /g' > ~/.config/rclone/bak_list.txt
+    rclone lsf "$my_remote":/ --include 'jellyfin_bak*' --files-only -F "pt" | sed 's/ /_/g;s/\;/    /g' > ~/.config/rclone/bak_list.txt
     bak_list=($(cat ~/.config/rclone/bak_list.txt))
     bak_name=$(whiptail --clear --ok-button "选择完毕,进入下一步" --backtitle "Hi,欢迎使用。有关脚本问题，请访问: https://github.com/cgkings/script-store 或者 https://t.me/cgking_s (TG 王大锤)。" --title "备份文件选择" --menu --nocancel "注：上下键回车选择,ESC退出脚本！" 18 62 10 \
     "${bak_list[@]}" 3>&1 1>&2 2>&3)
     if [ -z "$bak_name" ]; then
       rm -f ~/.config/rclone/bak_list.txt
       myexit 0
-    else
-      systemctl stop emby-server #结束 emby 进程
+  else
+      systemctl stop jellyfin-service #结束 jellyfin 进程
       rclone copy "$my_remote":"$bak_name" /root -vP
-      rm -rf /var/lib/emby
-      tar -xvf "/root/$bak_name" -C /var/lib && rm -f "$bak_name"
-      systemctl start emby-server
+      rm -rf /var/lib/jellyfin
+      tar -xvf "$bak_name" -C /var/lib && rm -f "$bak_name"
+      systemctl start jellyfin-service
       rm -rf ~/.config/rclone/bak_list.txt
-    fi
+      echo -e "${curr_date} [INFO] jellyfin还原完毕."
+  fi
 }
 
 ################## 卸载emby ##################
